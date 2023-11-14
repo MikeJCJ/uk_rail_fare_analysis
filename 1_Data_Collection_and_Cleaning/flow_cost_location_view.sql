e373ffadb5d7898f76e1cfa3f1c935b3b0f5a396
Mike Johnson <117915993+MikeJCJ@users.noreply.github.com>
2023-11-14 23:17:27 +0000

    Updates flow_cost_location_view.sql query
    This updated script uses the station_links.csv which contains all of the direct station to station links, to remove all routes which contain more than 2 stations.
    
    This script also includes the distance data between stations

--- 1_Data_Collection_and_Cleaning/flow_cost_location_view.sql
+++ 1_Data_Collection_and_Cleaning/flow_cost_location_view.sql
@@ -1,29 +1,39 @@
--- This query creates a view that has each flow, their stations names, and location for single adult fares
 SELECT
-    T1.FLOW_ID,
-    T4.TICKET_CODE,
-    T1.ORIGIN_CODE,
-    T1.DESTINATION_CODE,
-    T2.name AS ORIGIN_NAME,
-    T3.name AS DESTINATION_NAME,
-    T2.latitude AS ORIGIN_LAT,
-    T2.longitude AS ORIGIN_LON,
-    T3.latitude AS DESTINATION_LAT,
-    T3.longitude AS DESTINATION_LON,
-    CAST(T4.FARE AS FLOAT)/100 AS FARE_POUNDS
+    T4.FLOW_ID,
+    T5.TICKET_CODE,
+    T2.NLC_CODE AS ORIGIN_CODE,
+    T3.NLC_CODE AS DESTINATION_CODE,
+    T6.name AS ORIGIN_NAME,
+    T7.name AS DESTINATION_NAME,
+    T6.latitude AS ORIGIN_LAT,
+    T6.longitude AS ORIGIN_LON,
+    T7.latitude AS DESTINATION_LAT,
+    T7.longitude AS DESTINATION_LON,
+    CAST(T5.FARE AS FLOAT)/100 AS FARE_POUNDS,
+    T1.DISTANCE,
+    (CAST(T5.FARE AS FLOAT)/100)/CAST(T1.DISTANCE AS FLOAT) AS POUND_PER_MILE
 
-FROM flow_data_20230829 T1
+FROM station_links_20230829 T1 --Station to Station direct links
+LEFT JOIN station_code_lookup T2
+ON T1.ORIGIN_STATION=T2.CRS_CODE
 
-LEFT JOIN station_coords T2 --ORIGIN STATIONS
-ON T1.ORIGIN_CODE=SUBSTR(T2.uic,3,4)
+LEFT JOIN station_code_lookup T3 --Station code lookup
+ON T1.DESTINATION_STATION=T3.CRS_CODE
 
-LEFT JOIN station_coords T3 --DESTINATION STATIONS
-ON T1.DESTINATION_CODE=SUBSTR(T3.uic,3,4)
+LEFT JOIN flow_data_20230829 T4 --Station to Station flow ID lookup
+ON T2.NLC_CODE=T4.ORIGIN_CODE
+    AND T3.NLC_CODE=T4.DESTINATION_CODE
+    
+LEFT JOIN fare_data_cleaned T5 --Fare data linked to routes through flow ID
+ON T4.FLOW_ID=T5.FLOW_ID
 
-LEFT JOIN fare_data_20230829 T4
-ON T1.FLOW_ID=T4.FLOW_ID
+LEFT JOIN station_coords T6 --ORIGIN STATIONS' NAMES
+ON T2.NLC_CODE=SUBSTR(T6.uic,3,4)
 
-WHERE ORIGIN_NAME IS NOT NULL
-    AND DESTINATION_NAME IS NOT NULL
-    AND TICKET_CODE = 'SDS' --Anytime Day Single
+LEFT JOIN station_coords T7 --DESTINATION STATIONS' NAMES
+ON T3.NLC_CODE=SUBSTR(T7.uic,3,4)
+
+
+WHERE T4.FLOW_ID IS NOT NULL
+    AND TICKET_CODE='SDS' --Anytime Day Single
 ;
